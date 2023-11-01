1b6bbe64aeb2e1c9c5294bf66b84f872c49ebe32
vladislavikys <zarya.vlad@yandex.ru>
2023-11-01 15:44:17 +0300

    Initial Commit

---
+++ PlaneIt-ToDo-Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaneIt-ToDo-Code
+//
+//  Created by Влад on 1.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
